2221b35b7a6ffe080a70f29174f7ff766a545e3c
Rushikesh Janwe <90002061+rushikeshjanwe@users.noreply.github.com>
2026-01-14 18:38:53 +0530

    Update KafkaSimpleApplication.java

--- kafka-simple/src/main/java/com/example/kafka/KafkaSimpleApplication.java
+++ kafka-simple/src/main/java/com/example/kafka/KafkaSimpleApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-
-
 /**
  * =============================================================================
  * MAIN APPLICATION CLASS
